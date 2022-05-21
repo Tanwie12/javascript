4ce538e28aed992725f1c9c3977ced769c7425b2
Tanwie12 <adeytanwie@gmailcom>
2022-05-21 21:46:50 +0100

    edited with pretier

--- form.js
+++ form.js
@@ -1,99 +1,92 @@
-const credentials= require("./credentials")
+const prettier = require("prettier");
+prettier.format(form.js, js);
+const credentials = require("./credentials");
 const express = require("express");
 //const cookie=require("cookie-parser");
-const session=require("express-session");
-const store=new session.MemoryStore();
-var handlebars = require("express-handlebars")
-    .create({defaultLayout:'main',  
+const session = require("express-session");
+const store = new session.MemoryStore();
+var handlebars = require("express-handlebars").create({
+  defaultLayout: "main",
 });
 
-let app= express();
+let app = express();
 
-
-app.use(session({
+app.use(
+  session({
     store,
-    secret: 'secret',
+    secret: "secret",
     saveUninitialized: false,
-
-
-                 
-
-}))
-app.use(express.urlencoded({ extended:true}))
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine('handlebars',handlebars.engine)
-app.set('view engine', 'handlebars')
-app.use(express.static(__dirname + '/public'));
-let confirm=''
-app.get('/', function(req, res){
-    res.render('home')
-})
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+let confirm = "";
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
-
-var a=9;
-var b=10;
+var a = 9;
+var b = 10;
 const requireJsonContent = (request, response, next) => {
-    if (a==b) {
-        console.log('1',request.headers['content-type'])
-        response.status(400).send('Server requires application/json')
+  if (a == b) {
+    console.log("1", request.headers["content-type"]);
+    response.status(400).send("Server requires application/json");
+  } else {
+    //console.log('1a')
+    next();
+  }
+};
+app.post("/process-form", requireJsonContent, function (req, res) {
+  confirm = req.headers["content-type"];
+  //res.type('application/json')
+  //console.log('2',confirm)
+  //console.log("form submitted")
+  const { username, password } = req.body;
+
+  if (req.session.authenticated) {
+    res.redirect("/");
+  } else {
+    if (password == "123") {
+      req.session.authenticated = true;
+      req.session.user = {
+        username,
+        password,
+      };
+      res.cookie("user", req.sessionID);
+      console.log(req.session);
+      console.log(req.cookies);
+      console.log(store);
+      // res.json(store)
+      console.log(store);
     } else {
-        //console.log('1a')
-      next()
+      res.json("wrong password");
     }
   }
-app.post('/process-form',requireJsonContent,function(req, res){
-
-    confirm=req.headers['content-type'];
-    //res.type('application/json')
-      //console.log('2',confirm)
-      //console.log("form submitted")
-      const{ username, password}=req.body;
-      
-      if(req.session.authenticated){
-          res.redirect('/')
-      }else{
-          if(password=='123'){
-            req.session.authenticated=true
-              req.session.user={
-                  username,password
-              };
-              res.cookie('user', req.sessionID)
-              console.log(req.session)
-              console.log(req.cookies)
-              console.log(store)
-             // res.json(store)
-              console.log(store)
-
-          }else{
-              res.json('wrong password')
-          }
-      }
-     
-     
-})
+});
 
 //app.use('/form',validatecookies)
-app.get('/form',function(req, res){
-    //res.cookie('session_id','nom nom',{HttpOnly:true})
-   res.clearCookie('session_id')
-    res.render('form')
-})
- 
-app.get('/about',function(req, res){
-    console.log(store)
-    
-    if(req.session.authenticated){
-        res.render('about')
-    }else{
-        res.redirect('/form')
-    }
-    
-})
-app.get('/thank-you.html', function(req, res){
-    console.log(req.body.name);
-})
+app.get("/form", function (req, res) {
+  //res.cookie('session_id','nom nom',{HttpOnly:true})
+  res.clearCookie("session_id");
+  res.render("form");
+});
 
-app.listen(5500, function(req,res){
-    console.log('listening on 5500')
+app.get("/about", function (req, res) {
+  console.log(store);
 
-})
+  if (req.session.authenticated) {
+    res.render("about");
+  } else {
+    res.redirect("/form");
+  }
+});
+app.get("/thank-you.html", function (req, res) {
+  console.log(req.body.name);
+});
+
+app.listen(5500, function (req, res) {
+  console.log("listening on 5500");
+});
