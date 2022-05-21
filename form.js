338a970eae8beb8d298e8fedcf1abb9ccb5704cf
Tanwie12 <adeytanwie@gmailcom>
2022-05-21 17:22:36 +0100

    soem changes made

--- form.js
+++ form.js
@@ -20,33 +20,46 @@ app.get('/', function(req, res){
 
 function validatecookies(req,res,next) {
     const {cookies}=req
-    console.log(cookies)
-    next()
+    console.log(cookies.name)
+   if(cookies.name=='sessioning cookie'){
+       console.log("user validated")
+       next()
+   }else{
+       console.log("user not validated")
+       res.status(404).send(cookies.name)
+   }
+   
 
 }
+var a=9;
+var b=10;
 const requireJsonContent = (request, response, next) => {
-    if (request.headers['content-type'] !== 'application/json') {
+    if (a==b) {
         console.log('1',request.headers['content-type'])
         response.status(400).send('Server requires application/json')
     } else {
+        console.log('1a')
       next()
     }
   }
-app.use('/process-form',requireJsonContent)
-app.post('/process-form',function(req, res){
+app.post('/process-form',requireJsonContent,function(req, res){
     confirm=req.headers['content-type'];
     res.type('application/json')
       console.log('2',confirm)
       console.log("form submitted")
-       res.end();
+      res.redirect('/');
      
 })
-app.get('/form', validatecookies,function(req, res){
+app.use('/form',validatecookies)
+app.get('/form',function(req, res){
     res.cookie('session_id','nom nom',{HttpOnly:true})
     res.render('form')
 })
-
-
+ 
+app.get('/about',function(req, res){
+    res.cookie('name','sessioning cookie')
+    res.render('about')
+})
 app.get('/thank-you.html', function(req, res){
     console.log(req.body.name);
 })
