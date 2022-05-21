b6b2c2914142b05b4d753347c73da57b1afad229
Tanwie12 <adeytanwie@gmailcom>
2022-05-21 21:12:31 +0100

    using sessions

--- form.js
+++ form.js
@@ -1,13 +1,24 @@
 const credentials= require("./credentials")
 const express = require("express");
-const cookie=require("cookie-parser");
+//const cookie=require("cookie-parser");
+const session=require("express-session");
+const store=new session.MemoryStore();
 var handlebars = require("express-handlebars")
     .create({defaultLayout:'main',  
 });
 
 let app= express();
 
-app.use(cookie('secret','credentials.cookiesecret'))
+
+app.use(session({
+    store,
+    secret: 'secret',
+    saveUninitialized: false,
+
+
+                 
+
+}))
 app.use(express.urlencoded({ extended:true}))
 app.use(express.json());
 app.engine('handlebars',handlebars.engine)
@@ -18,19 +29,7 @@ app.get('/', function(req, res){
     res.render('home')
 })
 
-function validatecookies(req,res,next) {
-    const {cookies}=req
-    console.log(cookies.name)
-   if(cookies.name=='sessioning cookie'){
-       console.log("user validated")
-       next()
-   }else{
-       console.log("user not validated")
-       res.status(404).send(cookies.name)
-   }
-   
 
-}
 var a=9;
 var b=10;
 const requireJsonContent = (request, response, next) => {
@@ -38,27 +37,57 @@ const requireJsonContent = (request, response, next) => {
         console.log('1',request.headers['content-type'])
         response.status(400).send('Server requires application/json')
     } else {
-        console.log('1a')
+        //console.log('1a')
       next()
     }
   }
 app.post('/process-form',requireJsonContent,function(req, res){
+
     confirm=req.headers['content-type'];
-    res.type('application/json')
-      console.log('2',confirm)
-      console.log("form submitted")
-      res.redirect('/');
+    //res.type('application/json')
+      //console.log('2',confirm)
+      //console.log("form submitted")
+      const{ username, password}=req.body;
+      
+      if(req.session.authenticated){
+          res.redirect('/')
+      }else{
+          if(password=='123'){
+            req.session.authenticated=true
+              req.session.user={
+                  username,password
+              };
+              res.cookie('user', req.sessionID)
+              console.log(req.session)
+              console.log(req.cookies)
+              console.log(store)
+             // res.json(store)
+              console.log(store)
+
+          }else{
+              res.json('wrong password')
+          }
+      }
+     
      
 })
-app.use('/form',validatecookies)
+
+//app.use('/form',validatecookies)
 app.get('/form',function(req, res){
-    res.cookie('session_id','nom nom',{HttpOnly:true})
+    //res.cookie('session_id','nom nom',{HttpOnly:true})
+   res.clearCookie('session_id')
     res.render('form')
 })
  
 app.get('/about',function(req, res){
-    res.cookie('name','sessioning cookie')
-    res.render('about')
+    console.log(store)
+    
+    if(req.session.authenticated){
+        res.render('about')
+    }else{
+        res.redirect('/form')
+    }
+    
 })
 app.get('/thank-you.html', function(req, res){
     console.log(req.body.name);
